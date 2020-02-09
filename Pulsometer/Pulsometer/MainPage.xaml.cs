4e33fae6611526b386ef987846868bfc4676cb41
Tijana <tijana.ilic87@protonmail.com>
2020-02-09 03:07:12 +0100

    Create CheckPrivileges()

--- Pulsometer/Pulsometer/MainPage.xaml.cs
+++ Pulsometer/Pulsometer/MainPage.xaml.cs
@@ -8,6 +8,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 using Tizen.Wearable.CircularUI.Forms;
+using Tizen.Security;
 
 namespace Pulsometer
 {
@@ -17,9 +18,55 @@ namespace Pulsometer
         public MainPage()
         {
             InitializeComponent();
+            CheckPrivileges();
+        }     
+        
+        private void OnActionButtonClicked(object sender, EventArgs e)
+        {
+
         }
 
-        private void OnActionButtonClicked(object sender, EventArgs e)
+        private void CheckPrivileges()
+        {
+            // Check permission status (allow, deny, ask) to determinate action which has to be taken
+            string privilege = "http://tizen.org.privilege/healthinfo";
+            CheckResult result = PrivacyPrivilegeManager.CheckPermission(privilege);
+
+            if (result == CheckResult.Allow)
+            {
+                OnPrivilegesGranted();
+            }
+            else if (result == CheckResult.Deny)
+            {
+                OnPrivilegesDenied();
+            }
+            else // the user must be asked about granting the privilege
+            {
+                PrivacyPrivilegeManager.GetResponseContext(privilege).TryGetTarget(out var context);
+
+                if (context != null)
+                {
+                    context.ResponseFetched += (sender, e) =>
+                    {
+                        if (e.cause == CallCause.Answer && e.result == RequestResult.AllowForever)
+                        {
+                            OnPrivilegesGranted();
+                        }
+                        else
+                        {
+                            OnPrivilegesDenied();
+                        }
+                    };
+                }
+            }
+        }
+
+        private void OnPrivilegesGranted()
+        {
+
+        }
+
+        private void OnPrivilegesDenied()
         {
 
         }
