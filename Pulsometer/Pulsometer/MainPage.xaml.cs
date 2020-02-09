e59a0a71f09b99855ad2b3b8c4dd5105ee65c754
Tijana <tijana.ilic87@protonmail.com>
2020-02-09 02:47:23 +0100

    Add healthinfo privilege

--- Pulsometer/Pulsometer/MainPage.xaml.cs
+++ Pulsometer/Pulsometer/MainPage.xaml.cs
@@ -7,14 +7,21 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using Tizen.Wearable.CircularUI.Forms;
+
 namespace Pulsometer
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage : CirclePage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void OnActionButtonClicked(object sender, EventArgs e)
+        {
+
         }
     }
 }
